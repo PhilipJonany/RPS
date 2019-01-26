e02202aa5f3967422fa2ffe1c5fddc8304d00412
PhilipJonany <44513086+PhilipJonany@users.noreply.github.com>
2019-01-26 14:20:06 -0800

    Add files via upload

---
+++ RPS.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class RPS {
+   public static void main(String[] args) {
+      Scanner console = new Scanner(System.in);
+      String name = returnName(console);
+      
+      start(name);
+      
+   }
+   
+   public static String returnName(Scanner console) {
+      System.out.print("Welcome to Philip' RPS program. Before we start, what is your name? > ");
+      return console.next();
+   }
+   
+   public static void start(String name) {
+      System.out.println("Okay, " + name + ". Let's begin.");
+      System.out.println("Choose your weapon (R, P or S)");
+      
+   } 
+   
+   
+   
+}
